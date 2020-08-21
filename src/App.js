27aa6fb1208f3a40bed0a526b1caf94455259d7c
sanefc23 <san.efc@gmail.com>
2020-08-20 21:40:23 -0300

    implementando react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from './components/Navbar/CustomNavbar';
@@ -6,12 +7,16 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <CustomNavbar></CustomNavbar>
-      <Home
-        greeting="Bienvenido a House of Music"
-      />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home
+            greeting="Bienvenido a House of Music"
+          />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
