023d2a132a43b2af0deece79cdaaea6a9cc40003
sanefc23 <san.efc@gmail.com>
2020-09-13 20:27:54 -0300

    fix: cuando se agrega un album ya existente, actualiza las unidades, no un item nuevo

--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -8,15 +8,20 @@ export function CartProvider({ initialValue = [], children }) {
     const [albums, setAlbums] = useState(initialValue);
 
     function addAlbum(newAlbum) {
-        const a = [...albums, newAlbum];
-        setAlbums(a);
-        console.log("THIS ARE ALL THE ALBUMS: ", a);
+        const exists = albums.findIndex(existingAlbum => existingAlbum.id === newAlbum.id);
+        if (exists !== -1) {
+            albums[exists] = { ...newAlbum, units: albums[exists].units + newAlbum.units };
+            const a = [...albums];
+            setAlbums(a);
+        } else {
+            const a = [...albums, newAlbum];
+            setAlbums(a);
+        }
     }
 
     function removeAlbum(selectedAlbum) {
         const a = albums.filter(a => a.id !== selectedAlbum.id);
         setAlbums(a);
-        console.log('album removed');
     }
 
     return <CartContext.Provider value={{ albums, addAlbum, removeAlbum }}>
